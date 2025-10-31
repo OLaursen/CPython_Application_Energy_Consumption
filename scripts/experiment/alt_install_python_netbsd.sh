1a830ac4d4fe1d4736ba3741376c4bc41d35eeea
olau <olau@itu.dk>
2025-10-31 13:19:05 +0100

    Comments

--- scripts/experiment/alt_install_python_netbsd.sh
+++ scripts/experiment/alt_install_python_netbsd.sh
@@ -3,33 +3,33 @@ export PATH=$PATH:/usr/pkg/bin:/usr/pkg/sbin
 
 #Before running this script, ensure that a version of python is available and that psutil is manually installed and corrected:
 #### Python
-:'
+
     # Install build dependencies
-    pkgin -y update
-    doas pkgin -y in gmake git bash wget curl pkgconf libffi \
-        readline sqlite3 openssl zlib xz tk bzip2 libuuid \
-        gcc clang cmake autoconf automake libtool mpdecimal \
-        zstd 
+    # pkgin -y update
+    # doas pkgin -y in gmake git bash wget curl pkgconf libffi \
+    #     readline sqlite3 openssl zlib xz tk bzip2 libuuid \
+    #     gcc clang cmake autoconf automake libtool mpdecimal \
+    #     zstd 
 
-    # Install Python Seperate from experiement
-    doas pkgin in python39
-    python3.9 -m ensurepip
-    python3.9 -m pip install --upgrade pip
+    # # Install Python Seperate from experiement
+    # doas pkgin in python39
+    # python3.9 -m ensurepip
+    # python3.9 -m pip install --upgrade pip
 
-'
+
 
 #### Psutil
-:'
-    Update pkgin
-    ftp https://files.pythonhosted.org/packages/source/p/psutil/psutil-7.1.2.tar.gz
-    tar -xzf psutil-7.1.2.tar.gz
-    cd psutil-7.1.2
-'
+
+    # Update pkgin
+    # ftp https://files.pythonhosted.org/packages/source/p/psutil/psutil-7.1.2.tar.gz
+    # tar -xzf psutil-7.1.2.tar.gz
+    # cd psutil-7.1.2
+
 #Then within the "kinfo_getfile" method find the line "psutil_debug("exceeded INT_MAX")" and add the missing ";"
 #Then from within the psutil-7.1.2 directory run():
-:'
-    python3.9 -m pip install . --no-binary=:all:
-'
+
+#    python3.9 -m pip install . --no-binary=:all:
+
 
 python3.9 -m pip install pyperformance
 python3.9 -m pyperformance compile_all ./benchmark.conf
