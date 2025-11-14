557636303e9c6af436367998cd3ea1d7379ee702
olau <olau@itu.dk>
2025-11-14 01:45:04 +0100

    Moving away from compile_all

--- scripts/experiment/install_python_netbsd.sh
+++ scripts/experiment/install_python_netbsd.sh
@@ -6,22 +6,19 @@ doas pkgin -y update
 doas pkgin -y in gmake git bash wget curl pkgconf libffi \
         readline sqlite3 openssl zlib xz tk bzip2 libuuid \
         gcc clang cmake autoconf automake libtool mpdecimal \
-        zstd tcl
+        zstd tcl sudo python313 # At the time of writing it's 3.13.9
 
-# Install Python Seperate from experiement
-doas pkgin in python313 # At the time of writing it's 3.13.9
+# Install Python Separate from experiement
 doas python3.13 -m ensurepip
 doas python3.13 -m pip install --upgrade pip
 
-#Manually downloaded and corrected psutil repository
+# Manually downloaded and corrected psutil repository
 cd ~/psutil
 doas python3.13 -m pip install . --no-binary=:all:
 
-#Install sudo 
-doas pkgin -y install sudo
-
 #Download Cpython repo for benchmarks
 mkdir -p ~/pybench_results ~/pybench_builds
+
 cd
 git clone https://github.com/python/cpython.git
 cd cpython
@@ -32,11 +29,12 @@ export CPPFLAGS="-I/usr/pkg/include"
 export LDFLAGS="-L/usr/pkg/lib -Wl,-R/usr/pkg/lib"
 export PKG_CONFIG_PATH="/usr/pkg/lib/pkgconfig"
 export CFLAGS="-std=gnu99 -D_GNU_SOURCE"
-#Use all available cores
-export MAKEFLAGS="-j$(sysctl -n hw.ncpu)" 
 
+#Use all cpu cores
+export MAKEFLAGS="-j$(sysctl -n hw.ncpu)" 
 doas python3.13 -m pip install pyperformance pyperf
 doas python3.13 -m pyperformance compile_all ~/cpython_application_energy_consumption/scripts/experiment/benchmark.conf
-    
+
+mkdir -p ~/pybench_builds/
 echo which python3.13
 echo "Installation complete!"
