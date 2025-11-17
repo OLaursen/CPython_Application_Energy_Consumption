a6062f74f1cfb2f5eb32ff5972be4d631f36ebf7
olau <olau@itu.dk>
2025-11-17 14:44:19 +0100

    remove sphinx actually

--- scripts/experiment/run_benchmarks.sh
+++ scripts/experiment/run_benchmarks.sh
@@ -4,15 +4,17 @@
 # Function to run benchmarks on a specific Python version
 run_benchmarks() {
     PYTHON_BIN=$1
-    PYTHON_PATH=$2 
-    $PYTHON_BIN -m pyperformance run --benchmark=2to3,chameloen,docutils,html5lib,sphinx,tornado_http --python=$PYTHON_PATH -o $PYTHON_BIN.json
+    PYTHON_PATH=$2
+    $PYTHON_BIN -m pyperformance run --benchmark=2to3,chameleon,docutils,html5lib,tornado_http --python=$PYTHON_PATH -o $PYTHON_BIN.json
 }
 
 # Script finds path to desired Python version
 if command -v $1 &>/dev/null; then
     PYTHON_PATH=$($1 -c "import sys; print(sys.executable)")
     echo "Running benchmarks for $1 at $PYTHON_PATH..."
+    
     run_benchmarks $1 $PYTHON_PATH
+    
 else
     echo "$1 is not installed. Exiting."
 fi
