e9fb56098dc2e5f2a26bb2f99541d6d31d9b4dc4
olau <olau@itu.dk>
2025-12-30 09:17:27 +0100

    Apply patches in buildstep test

---
+++ scripts/experiment/tmp_install_script.bash
@@ -0,0 +1,124 @@
+#!/bin/sh
+
+export PATH="/usr/pkg/bin:/usr/pkg/sbin:/usr/local/bin:/usr/bin:/bin"
+install_python(){
+        VERSION=$1
+        MAJOR_MINOR=$(echo "$VERSION" | cut -d. -f1,2)
+        PYTHON_BIN="/usr/pkg/bin/python${MAJOR_MINOR}"
+        PYTHON_SRC="Python-$VERSION"
+        PYTHON_TGZ="$PYTHON_SRC.tgz"
+        BUILDDIR="$HOME/build/python${MAJOR_MINOR}"
+        echo "This is the build dir: $BUILDDIR"
+        # Check if Python version is already installed
+        # if [ -x "$PYTHON_BIN" ] && [ "$($PYTHON_BIN --version 2>&1)" = "Python $VERSION" ]; then
+        #     echo "$PYTHON_SRC is already installed."
+        #     return
+        # fi
+
+        echo "Fetching Python from source"
+        cd
+        mkdir -p $BUILDDIR
+        cd $BUILDDIR
+        echo "Downloading $PYTHON_SRC..."
+        ftp https://www.python.org/ftp/python/${VERSION}/${PYTHON_SRC}.tgz
+        tar -xzf "$PYTHON_TGZ"
+        cd "$PYTHON_SRC"
+        
+        #Ensures openssl can be found:
+        export CPPFLAGS="-I/usr/pkg/include"
+        export LDFLAGS="-L/usr/pkg/lib -Wl,-R/usr/pkg/lib"
+        export PKG_CONFIG_PATH="/usr/pkg/lib/pkgconfig"
+        export CFLAGS="-std=gnu99 -D_GNU_SOURCE"
+        
+        echo "_ctypes _ctypes/*.c" >> Modules/Setup.local
+        echo "CFLAGS+= -std=gnu99 -D_GNU_SOURCE" >> Modules/Setup.local
+        
+        echo "Configuring the build with optimizations..."
+        ./configure --prefix=/usr/local --enable-optimizations --with-ensurepip=upgrade --with-openssl=/usr/pkg --with-openssl-rpath=/usr/pkg/lib
+
+        echo "Building Python $VERSION"
+        gmake -j profile-opt
+        gmake altinstall
+        if [ ! -x "$PYTHON_BIN" ]; then
+                echo "Build or installation failed for Python $VERSION"
+        fi
+        
+        echo "Cleaning up build files..."
+        cd ..
+        rm -rf "$PYTHON_SRC" "$PYTHON_TGZ"
+
+        echo "Verifying installation..."
+        "python${MAJOR_MINOR}" --version
+        
+        echo "Installing pyperformance for $PYTHON_BIN..."
+        "python${MAJOR_MINOR}" -m pip install --upgrade pip
+        "python${MAJOR_MINOR}"  -m pip install pyperformance
+        
+        echo "Finished installing Python $VERSION"
+        date
+}
+# Install build dependencies
+doas pkgin -y update
+doas pkgin -y in gmake git bash wget curl pkgconf libffi \
+        readline sqlite3 openssl zlib xz tk bzip2 libuuid \
+        gcc clang cmake autoconf automake libtool mpdecimal \
+        zstd tcl sudo
+
+#install_python "3.10.18"
+install_python "3.13.7"
+#install_python "3.12.11"
+echo "Installation complete!"#!/bin/sh
+
+# export PATH="/usr/pkg/bin:/usr/pkg/sbin:/usr/local/bin:/usr/bin:/bin"
+# install_python(){
+#         VERSION=$1
+#         MAJOR_MINOR=$(echo "$VERSION" | cut -d. -f1,2)
+#         PYTHON_BIN="/usr/pkg/bin/python${MAJOR_MINOR}"
+#         PYTHON_SRC="Python-$VERSION"
+#         PYTHON_TGZ="$PYTHON_SRC.tgz"
+#         BUILDDIR="$HOME/build/python${MAJOR_MINOR}"
+#         echo "This is the build dir: $BUILDDIR"
+#         # Check if Python version is already installed
+#         # if [ -x "$PYTHON_BIN" ] && [ "$($PYTHON_BIN --version 2>&1)" = "Python $VERSION" ]; then
+#         #     echo "$PYTHON_SRC is already installed."
+#         #     return
+#         # fi
+
+#         echo "Fetching Python from source"
+#         cd
+#         mkdir -p $BUILDDIR
+#         cd $BUILDDIR
+#         echo "Downloading $PYTHON_SRC..."
+#         ftp https://www.python.org/ftp/python/${VERSION}/${PYTHON_SRC}.tgz
+#         tar -xzf "$PYTHON_TGZ"
+#         cd "$PYTHON_SRC"
+        
+#         #Ensures openssl can be found:
+#         export CPPFLAGS="-I/usr/pkg/include" export LDFLAGS="-L/usr/pkg/lib -Wl,-R/usr/pkg/lib" export PKG_CONFIG_PATH="/usr/pkg/lib/pkgconfig" export CFLAGS="-std=gnu99 -D_GNU_SOURCE"
+        
+#         echo "_ctypes _ctypes/*.c" >> Modules/Setup.local echo "CFLAGS+= -std=gnu99 -D_GNU_SOURCE" >> Modules/Setup.local
+        
+#         echo "Configuring the build with optimizations..." ./configure --prefix=/usr/local --enable-optimizations --with-ensurepip=upgrade --with-openssl=/usr/pkg --with-openssl-rpath=/usr/pkg/lib echo 
+#         "Building Python $VERSION" gmake -j profile-opt gmake altinstall if [ ! -x "$PYTHON_BIN" ]; then
+#                 echo "Build or installation failed for Python $VERSION" fi
+        
+#         echo "Cleaning up build files..." cd .. rm -rf "$PYTHON_SRC" "$PYTHON_TGZ" echo "Verifying installation..." "python${MAJOR_MINOR}" --version
+        
+#         echo "Installing pyperformance for $PYTHON_BIN..."
+#         "python${MAJOR_MINOR}" -m ip install --upgrade pip
+#         "python${MAJOR_MINOR}"  -m pip install pyperformance
+        
+#         echo "Finished installing Python $VERSION"
+#         date
+# }
+# # Install build dependencies
+# doas pkgin -y update
+# doas pkgin -y in gmake git bash wget curl pkgconf libffi \
+#         readline sqlite3 openssl zlib xz tk bzip2 libuuid \
+#         gcc clang cmake autoconf automake libtool mpdecimal \
+#         zstd tcl sudo
+
+# #install_python "3.10.18"
+# #install_python "3.13.9"
+# install_python "3.12.11"
+# echo "Installation complete!" 
