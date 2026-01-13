36af930906d79ae7aa63ac51178ec75b114ad5a0
olau <olau@itu.dk>
2026-01-13 08:46:28 +0100

    Small updates

--- scripts/experiment/install_python_debian.sh
+++ scripts/experiment/install_python_debian.sh
@@ -11,13 +11,13 @@ curl https://pyenv.run | bash
 # Set pyenv in path
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
-source ~/.bashr
+source ~/.bashrc
 
 # Install Python versions via pyenv
 VERSIONS=("3.13.7" "3.12.11" "3.11.13" "3.10.18" "3.9.23")
 for VERSION in "${VERSIONS[@]}"; do
     PYTHON_BIN="~/.pyenv/versions/$VERSION/bin/python"
-    $PYTHON_BIN install $VERSION
+    $PYTHON_BIN install -s $VERSION
     $PYTHON_BIN -m pip install pyperformance
 
 done
