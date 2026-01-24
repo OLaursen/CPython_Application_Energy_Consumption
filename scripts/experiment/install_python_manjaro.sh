d74330cd24d46b6184e2c81f89cfd5cae2598532
olau <olau@itu.dk>
2026-01-24 19:06:12 +0100

    Manjaro install script

---
+++ scripts/experiment/install_python_manjaro.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#install dependencies
+sudo pacman -S --needed bash base-devel git openssl zlib xz libffi bzip2 readline sqlite tk ncurses gdbm libnsl
+
+curl https://pyenv.run | bash
+
+# Set pyenv in path
+cat << 'EOF' >> ~/.bashrc
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init --path)"
+  eval "$(pyenv init -)"
+fi
+EOF
+
+source ~/.bashrc
+
+# Install Python versions via pyenv
+VERSIONS=("3.13.7" "3.12.11" "3.11.13" "3.10.18" "3.9.23")
+for VERSION in "${VERSIONS[@]}"; do
+    PYTHON_BIN="$HOME/.pyenv/versions/$VERSION/bin/python"
+    pyenv install $VERSION
+    $PYTHON_BIN -m pip install pyperformance
+
+done
+
+# Find project directory or clone it
+if [[ -d "$HOME/CPython_Application_Energy_Consumption" ]]; then
+    echo "Project directory found. Checking for updates."
+    cd "$HOME/CPython_Application_Energy_Consumption"
+    git fetch -a
+    git pull
+else
+  echo "Project directory not found, cloning repository into HOME directory."
+  cd 
+  git clone "https://github.com/olaursen/CPython_Application_Energy_Consumption.git"
+fi
+
+echo "Python installation and setup complete!"
