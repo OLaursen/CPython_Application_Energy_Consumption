b213bf2f48f4c1d2bfff3b55f89e876ce171fd79
olau <olau@itu.dk>
2026-01-25 08:19:30 +0100

    Unclean install script for netbsd

--- scripts/experiment/install_python_netbsd.sh
+++ scripts/experiment/install_python_netbsd.sh
@@ -1,87 +1,24 @@
 #!/bin/sh
 
-export PATH="/usr/pkg/bin:/usr/pkg/sbin:/usr/local/bin:/usr/bin:/bin"
-install_python(){
-        VERSION=$1
-        MAJOR_MINOR=$(echo "$VERSION" | cut -d. -f1,2)
-        PYTHON_BIN="/usr/pkg/bin/python${MAJOR_MINOR}"
-        PYTHON_SRC="Python-$VERSION"
-        PYTHON_TGZ="$PYTHON_SRC.tgz"
-        BUILDDIR="$HOME/build/python${MAJOR_MINOR}"
-        echo "This is the build dir: $BUILDDIR"
-        # Check if Python version is already installed
-        # if [ -x "$PYTHON_BIN" ] && [ "$($PYTHON_BIN --version 2>&1)" = "Python $VERSION" ]; then
-        #     echo "$PYTHON_SRC is already installed."
-        #     return
-        # fi
-
-        echo "Fetching Python from source"
-        cd
-        mkdir -p $BUILDDIR
-        cd $BUILDDIR
-        echo "Downloading $PYTHON_SRC..."
-        ftp https://www.python.org/ftp/python/${VERSION}/${PYTHON_SRC}.tgz
-        tar -xzf "$PYTHON_TGZ"
-        cd "$PYTHON_SRC"
-        
-        #Ensures openssl can be found:
-      
-        export PKG_CONFIG_PATH="/usr/pkg/lib/pkgconfig"
-        export CPPFLAGS="-I/usr/pkg/include"
-        export LDFLAGS="-L/usr/pkg/lib -Wl,-R/usr/pkg/lib"
-        export CFLAGS="-std=gnu99 -D_GNU_SOURCE"
-        # CPPFLAGS="-I/usr/pkg/include" \
-        # LDFLAGS="-L/usr/pkg/lib -Wl,-R/usr/pkg/lib"
-
-        
-        echo "Configuring the build with optimizations..."
-        ./configure --prefix=/usr/local \
-                --enable-optimizations \
-                --with-ensurepip=upgrade \
-                --with-openssl=/usr/pkg \
-                --with-openssl-rpath=/usr/pkg/lib \
-
-
-        echo "Building Python $VERSION"
-        gmake -j$(sysctl -n hw.ncpu) profile-opt
-        gmake altinstall
-        if [ ! -x "$PYTHON_BIN" ]; then
-                echo "Build or installation failed for Python $VERSION"
-        fi
-        
-        echo "Cleaning up build files..."
-        cd ..
-        rm -rf "$PYTHON_SRC" "$PYTHON_TGZ"
-
-        echo "Verifying installation..."
-        "python${MAJOR_MINOR}" --version
-        
-        echo "Installing pyperformance for $PYTHON_BIN..."
-        "python${MAJOR_MINOR}" -m pip install --upgrade pip
-        "python${MAJOR_MINOR}"  -m pip install pyperformance
-        
-        echo "Finished installing Python $VERSION"
-        date
-}
 # Install build dependencies
 doas pkgin -y update
 doas pkgin -y in gmake git bash wget curl pkgconf libffi \
         readline sqlite3 openssl zlib xz tk bzip2 libuuid \
         gcc clang cmake autoconf automake libtool mpdecimal \
         zstd tcl sudo ncurses
-
 # Install pyenv
-
+curl https://pyenv.run | bash
 
 # Set pyenv in path
-
 echo '
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
-' >> ~/.profile
-. ~/.profile
+' >> ~/.bashrc
+
+. ~/.bashrc
+
 
 pyenv --version
 # Install Python versions via pyenv
@@ -104,6 +41,24 @@ for VERSION in $VERSIONS; do
     "$PYTHON_BIN" -m pip install pyperformance
 done
 
+
+# Done in bash shell from home directory 
+# Set C build flags
+export CC=gcc
+export MAKE=gmake
+export PYTHON_CONFIGURE_OPTS="--with-openssl=/usr/pkg --with-openssl-rpath=auto"
+export CPPFLAGS="-I/usr/pkg/include -I/usr/pkg/include/ncurses"
+export LDFLAGS="-L/usr/pkg/lib -Wl,-R/usr/pkg/lib"
+export PKG_CONFIG_PATH="/usr/pkg/lib/pkgconfig"
+export CFLAGS="${CFLAGS:+$CFLAGS }-std=gnu11"
+
+# Assumes that patch folder from pkgsrc repositiory are extracted to home directory named patches<major-version><minor-version>
+cat patches313/* | pyenv install -v --patch 3.13.7
+cat patches312/* | pyenv install -v --patch 3.12.11
+cat patches311/* | pyenv install -v --patch 3.11.13
+cat patches310/* | pyenv install -v --patch 3.10.18
+cat patches39/* | pyenv install -v --patch 3.9.23
+
 # Find project directory or clone it
 if [[ -d "$HOME/CPython_Application_Energy_Consumption" ]]; then
     echo "Project directory found. Checking for updates."
@@ -117,20 +72,3 @@ else
 fi
 
 echo "Python installation and setup complete!"
-
-CPPFLAGS="-I/etc/openssl/include" \
-LDFLAGS="-L/etc/openssl/lib" \
-pyenv install -v -s "3.13.7"
-
-# Applying a patch to pyenv
-#Assuming that patches are in "~/patchesXXX"
-cd 
-cd patches313
-patches=""
-
-for file in *.patch; do
-    echo "Applying patch $file"
-    patches="$patches$file "
-done
-
-echo "All patches to apply: $patches"
