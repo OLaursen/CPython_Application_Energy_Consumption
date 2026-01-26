e213216f5b61c2f5936ddf892a6773dba8d12469
olau <olau@itu.dk>
2026-01-26 10:31:06 +0100

    slim down benchmark.sh

--- scripts/experiment/run_benchmarks.sh
+++ scripts/experiment/run_benchmarks.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# Run this script with desired Python Versions as Argument (e.g., ./run_benchmarks.sh 3.9.23)
 
-# Function to run benchmarks on a specific Python version
-run_benchmarks() {
-    PYENV=$1
-    PYTHON_BIN="$HOME/.pyenv/versions/$PYENV/bin/python"
-    $PYTHON_BIN -m pyperformance run --benchmark=2to3,docutils,html5lib,tornado_http,async_generators,float,nbody,pidigits,regex_compile,json_dumps,json_loads,pickle_list,python_startup -o $PYENV.json
-}
+# Run benchmarks for a specified Python version using pyperformance
+# ./run_benchmarks.sh <python-version>
+# Example usage: ./run_benchmarks.sh 3.10.18 
 
-# Script finds path to desired Python version
-#Make it so that it finds the correct pyenv binary
-run_benchmarks $1
 
-echo "Benchmarking complete for $1."
+PYTHON_VERSION=$1
+PYTHON_BIN="$HOME/.pyenv/versions/$PYTHON_VERSION/bin/python"
+# Check if pyenv with given version is installed as well as pyperformance
+if ! pyenv versions --bare | grep -q "^$PYTHON_VERSION$"; then
+    echo "Python version $PYTHON_VERSION is not installed via pyenv."
+    exit 1
+fi
+
+
+$PYTHON_BIN -m pyperformance run --benchmark=2to3,docutils,html5lib,tornado_http,async_generators,float,nbody,pidigits,regex_compile,json_dumps,json_loads,pickle_list,python_startup -o $PYTHON_VERSION.json
+echo "Benchmarking complete for $PYTHON_VERSION."
